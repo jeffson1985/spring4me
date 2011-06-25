81f257d95c1b049eb14413b056f62369d0cdacfe
gavin.hu@opensourceforce.org <gavin.hu@opensourceforce.org@531c5ff9-fd78-5658-fc8f-1ac724a7caa4>
2011-06-25 08:36:55 +0000

    [maven-release-plugin]  copy for tag spring4me-0.2.2

---
+++ tags/spring4me-0.2.2/spring4me-demo/src/main/java/org/osforce/spring4me/demo/widget/ContentWidget.java
@@ -0,0 +1,28 @@
+package org.osforce.spring4me.demo.widget;
+
+import org.osforce.spring4me.web.bind.annotation.PrefParam;
+import org.osforce.spring4me.web.stereotype.Widget;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author <a href="mailto:haozhonghu@hotmail.com">gavin</a>
+ * @since 0.1.0
+ * @create May 16, 2011 - 3:52:47 PM
+ * <a href="http://www.opensourceforce.org">开源力量</a>
+ */
+@Widget
+@RequestMapping("/commons/content")
+public class ContentWidget {
+
+	public ContentWidget() {
+	}
+
+	@RequestMapping(value="/detail-view")
+	public String test(@PrefParam String content, Model model) {
+		model.addAttribute("content", content);
+		return "commons/content";
+	}
+
+}
